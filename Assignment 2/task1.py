1e00071198652e132ba48da6f2f1450b64981ba0
chiragshah101902 <chiragshah101902@gmail.com>
2025-03-09 03:22:04 +0530

    Update Assignment 2 - task 1

---
+++ Assignment 2/task1.py
@@ -0,0 +1,6 @@
+inputNumber = int(input("Enter a number: "))
+
+if inputNumber%2 == 0:
+    print(inputNumber, "is an even number")
+elif inputNumber%2 != 0:
+    print(inputNumber, "is an odd number")
